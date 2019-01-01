4b45d1683b8ec4112218cf763761d16971702229
Sam <samsung@alumni.ubc.ca>
2019-01-01 02:24:32 -0800

    added minimal collision detection

--- world.py
+++ world.py
@@ -5,12 +5,13 @@ class Racer():
         self.position=(0,0)
         self.velocity=(0,0)
         self.direction=0
+        self.lap=0
         self.instruction=None
 
 class World():
     def __init__(self):
-        map_width=1000
         map_height=500
+        map_width=1000
         self.racers=[]
         self.world_map=np.zeros([map_height, map_width], dtype=np.uint8)
         
@@ -27,6 +28,12 @@ class World():
         for racer in self.racers:
             self.update_racer(racer)
     
-    def update_racers(self, racer):
-        #TODO
+    def update_racer(self, racer):
+        # TODO
         pass
+        
+    # Return True if collision occur, else return False
+    def check_collision(self, old_position, new_position):
+        if self.world_map[new_position]!=0:
+            return True
+        return False
