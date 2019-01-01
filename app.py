ad8aba3ab4961a987ad555fed6fdd89e83ba6a85
edwardyh <edward.yh.huang@outlook.com>
2019-01-01 02:24:16 -0800

    Removed more things

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import matplotlib.animation as animation
 
 from world import World
 
-RESOLUTION = 1
-
 world = World()
 
 def gameUpdate(*args):
@@ -14,7 +12,6 @@ def gameUpdate(*args):
     return im,
 
 screen = plt.figure()
-
 im = plt.imshow(world.world_map, cmap='gray', vmin=0, vmax=255, animated=True)
 ani = animation.FuncAnimation(screen, gameUpdate, frames=1000, interval=1, blit=False)
 plt.show()
