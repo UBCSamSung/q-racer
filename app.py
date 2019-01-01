d0d817481a8385e2987295592c153f9676995a63
edwardyh <edward.yh.huang@outlook.com>
2019-01-01 02:39:38 -0800

    Tidy up app

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-import threading
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-
 from world import World
 
+# Initialize new world
 world = World()
 
+# Game update
 def gameUpdate(*args):
     im.set_data(world.world_map)
     return im,
 
+# Initialize renderer
 screen = plt.figure()
 im = plt.imshow(world.world_map, cmap='gray', vmin=0, vmax=255, animated=True)
 ani = animation.FuncAnimation(screen, gameUpdate, frames=1000, interval=1, blit=False)
