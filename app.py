701a8e1416d3dd7849e886b34e8dfc1b2405c66d
Sam <samsung@alumni.ubc.ca>
2019-01-01 19:59:34 -0800

    added game restart

--- app.py
+++ app.py
@@ -5,29 +5,36 @@ from world import World
 from test_maps import rectangle, straight
 from Agent import Agent
 
+def main():
 
-# Initialize new world
-world = World()
-world.set_map(straight)
-#world.set_map(rectangle)
+    # Initialize new world
+    world = World()
+    # world.set_map(straight)
+    world.set_map(rectangle)
 
-# For demo:
-world.add_racer(0)
-world.racers[0].velocity=(0,1)
+    # For demo:
+    agents = []
+    for _ in range(100):
+        agents.append(Agent())
+    for agent in agents:
+        world.add_racer(0)
 
-agents = []
-agents.append(Agent())
+    # Game update
+    def gameUpdate(*args):
+        state = world.get_state()
+        actions=[agent.get_action(state) for agent in agents]
+        world.update(actions)
+        im.set_data(world.world_map)
+        return im,
 
-# Game update
-def gameUpdate(*args):
-    state = world.get_state()
-    actions=[agent.get_action(state) for agent in agents]
-    world.update(actions)
-    im.set_data(world.world_map)
-    return im,
+    # Initialize renderer
+    screen = plt.figure()
+    im = plt.imshow(world.world_map, cmap='gray', vmin=0, vmax=255, animated=True)
+    ani = animation.FuncAnimation(screen, gameUpdate, frames=1000, interval=30, blit=False)
+    plt.show()
 
-# Initialize renderer
-screen = plt.figure()
-im = plt.imshow(world.world_map, cmap='gray', vmin=0, vmax=255, animated=True)
-ani = animation.FuncAnimation(screen, gameUpdate, frames=1000, interval=30, blit=False)
-plt.show()
+if __name__ == "__main__":
+    # Number of time to run
+    repeat=3
+    for _ in range(repeat):
+        main()
