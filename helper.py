97f3a061e7d8d1061921a30e009513d8bae85f7f
Sam <samsung@alumni.ubc.ca>
2019-01-01 16:07:35 -0800

    add helper function to create line equation

--- helper.py
+++ helper.py
@@ -12,4 +12,11 @@ def angle2vector(angle):
     return rotate([1,0], angle)
 
 def vector2angle(vector):
-    return math.degrees(math.atan2(vector[1], vector[0]))    
+    return math.degrees(math.atan2(vector[1], vector[0]))
+
+# Return a function that take x position and return y position
+def point2line(p1, p2, inverse=False):
+    if inverse:
+        p1=p1[::-1]
+        p2=p2[::-1]
+    return lambda x: (p2[0]-p1[0])/(p2[1]-p1[1])*(x-p1[1])+p1[0]
